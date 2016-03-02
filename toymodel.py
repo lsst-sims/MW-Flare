5b533a343b3710477eb7109de2012e08f21f6a07
James Davenport <jradavenport@gmail.com>
2016-03-01 17:39:05 -0800

    add method for making rough cadences

--- toymodel.py
+++ toymodel.py
@@ -3,8 +3,10 @@ A toy model for generating flares along a given line-of-sight in LSST
 
 '''
 
+
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 '''
@@ -16,6 +18,48 @@ Things I'll need:
 - LSST cadence, or cadence approximation
 - Kepler-based SpT vs Flare Rate model (Davenport in prep)
 
-
 '''
+
+
+
+def generate_visits(Nvisits=900, tspan=10, stat=False):
+    '''
+    Use some very crude approximations for how visits will be spaced out:
+
+    - Survey starts at midnight, time = 0.0
+    - Can only observe at night, time > 0.75 | time < 0.25
+    - Exposures are clustered around a season w/ a gaussian shape each year
+    - Field is observable for first half of year, 0 < date < 182
+    - On average, each field is hit every 3 days during observable season
+
+    '''
+
+    # generate random times for visit, between [0.75 and 0.25]
+    time_of_day = np.random.random(Nvisits)/2. - 0.25
+
+    date_of_year = np.floor(np.random.normal(loc=365./4., scale=365./7., size=Nvisits))
+
+    year_of_obs = np.floor(np.random.random(Nvisits) * tspan) * 365.
+
+    date_obs = time_of_day + date_of_year + year_of_obs
+
+    date_obs.sort()
+
+    if stat is True:
+        print('mean time between visits:')
+        print(np.mean(date_obs[1:] - date_obs[:-1]))
+
+        print('median time between visits:')
+        print(np.median(date_obs[1:] - date_obs[:-1]))
+
+        plt.figure()
+        _ = plt.hist(date_obs, bins=np.arange(date_obs.min(), date_obs.max(),7),
+                     histtype='stepfilled', color='k')
+        plt.xlabel('Time (days)')
+        plt.ylabel('# Visits per Week')
+        plt.show()
+
+    return date_obs
+
+
 
