625a8f393a140c09fa4451cb61abec116456cf53
James Davenport <jradavenport@gmail.com>
2016-03-01 22:08:29 -0800

    update model list

--- toymodel.py
+++ toymodel.py
@@ -1,15 +1,6 @@
 '''
 A toy model for generating flares along a given line-of-sight in LSST
 
-'''
-
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
-'''
 Things I'll need:
 - fields of view to compute over
     - use Trilegal fields for now
@@ -21,8 +12,16 @@ Things I'll need:
 '''
 
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def generate_visits(Nvisits=900, tspan=10, stat=False):
+# flare morphology, taken from "appaloosa" (Davenport 2014)
+import aflare
+
+
+def generate_visits(Nvisits=900, tspan=10, stat=False,
+                    seasonscale=365./5):
     '''
     Use some very crude approximations for how visits will be spaced out:
 
@@ -30,14 +29,16 @@ def generate_visits(Nvisits=900, tspan=10, stat=False):
     - Can only observe at night, time > 0.75 | time < 0.25
     - Exposures are clustered around a season w/ a gaussian shape each year
     - Field is observable for first half of year, 0 < date < 182
-    - On average, each field is hit every 3 days during observable season
+    - On average, each field should be hit every 3 days during observable season
+
+    Set "stat=True" if you want a plot and a couple basic statistics about the cadence
 
     '''
 
     # generate random times for visit, between [0.75 and 0.25]
     time_of_day = np.random.random(Nvisits)/2. - 0.25
 
-    date_of_year = np.floor(np.random.normal(loc=365./4., scale=365./7., size=Nvisits))
+    date_of_year = np.floor(np.random.normal(loc=365./4., scale=seasonscale, size=Nvisits))
 
     year_of_obs = np.floor(np.random.random(Nvisits) * tspan) * 365.
 
@@ -62,4 +63,29 @@ def generate_visits(Nvisits=900, tspan=10, stat=False):
     return date_obs
 
 
+def run_field(file):
+    '''
+    for this TRILEGAL field:
+    - generate a cadence model
+    - for every star generate simulated flares
 
+    '''
+
+    df = pd.read_table(file)
+
+    return
+
+
+def all_fields(models='index.txt'):
+    dir = 'trilegal_models/'
+    files = np.loadtxt(dir+models, comments='#', unpack=True, usecols=(0,), delimiter=',', dtype=np.str)
+
+    for k in range(len(files)):
+        print('running '+ dir + files[k])
+        run_field(dir+files[k])
+
+    return
+
+
+if __name__ == "__main__":
+    all_fields()
