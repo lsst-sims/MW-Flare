e7e31ba23357451bbbdf9c94494ee7676ddff036
danielsf <scott.f.daniel@gmail.com>
2017-01-31 16:33:43 -0800

    added unit test for vectorization of xyz_from_lon_lat_px

--- lsst_sims/test_mdwarf_utils.py
+++ lsst_sims/test_mdwarf_utils.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 
 from mdwarf_utils import prob_of_type
+from mdwarf_utils import xyz_from_lon_lat_px
 
 class ProbTestCase(unittest.TestCase):
 
@@ -24,6 +25,30 @@ class ProbTestCase(unittest.TestCase):
                 msg = 'failed on star %d; type %d' % (ix, mm)
                 self.assertAlmostEqual(vector_pdf[mm][ix]/single_pdf[mm],
                                        1.0, 10, msg=msg)
+
+
+class XyzTestCase(unittest.TestCase):
+
+    longMessage = True
+
+    def test_vectorize(self):
+        """
+        Test that vectorized xyz_from_lon_lat_px gives the same results
+        as xyz_from_lon_lat_px run one-at-a-time
+        """
+        rng = np.random.RandomState(813412)
+        n_stars = 20
+        gal_l = rng.random_sample(n_stars)*360.0
+        gal_b = rng.random_sample(n_stars)*180.0 - 90.0
+        px = rng.random_sample(n_stars)*1.0 + 0.0001
+
+        vector_xyz = xyz_from_lon_lat_px(gal_l, gal_b, px)
+
+        for ix in range(n_stars):
+            xyz = xyz_from_lon_lat_px(gal_l[ix], gal_b[ix], px[ix])
+            for ii in range(3):
+                msg = 'failed on star %d; dim %d' % (ix, ii)
+                self.assertEqual(xyz[ii], vector_xyz[ii][ix], msg=msg)
 
 if __name__ == "__main__":
     unittest.main()
