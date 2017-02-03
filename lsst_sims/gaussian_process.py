3e2f763f5a038ee4c89e101d8b32ba65e5b46900
danielsf <scott.f.daniel@gmail.com>
2017-02-03 12:15:55 -0800

    start implementing Gaussin Process; test ExpSquaredKernel

---
+++ lsst_sims/gaussian_process.py
@@ -0,0 +1,154 @@
+"""
+This script will read in the 'fraction of each spectral type that is variable
+in each bin in distance from the Galactic plane' data stored in z_bins/ and
+try to fit that data with a smooth curve using a Gaussian Process.
+"""
+
+import numpy as np
+import copy
+
+
+__all__ = ["KernelBase", "ExpSquaredKernel", "Covariogram"]
+
+
+class KernelBase(object):
+
+    @property
+    def n_hyper_params(self):
+        return self._n_hyper_params
+
+    @property
+    def hyper_params(self):
+        return self._hyper_params
+
+    @hyper_params.setter
+    def hyper_params(self, val):
+        if hasattr(val, '__len__'):
+            n_val = len(val)
+        else:
+            n_val = 1
+
+        if n_val != self._n_hyper_params:
+            raise RuntimeError("Trying to give "
+                               + "Kernel %d hyper params; " % n_val
+                               + "it expects %d" % self._n_hyper_params)
+
+        self._hyper_params = val
+
+    def __call__(self, pt1, pt2_list):
+        if isinstance(pt1, float) and len(pt2_list.shape) != 1:
+            raise RuntimeError("passed kernel a float "
+                               + "and a %d numpy array" % len(pt2_list.shape))
+
+        if isinstance(pt1, np.ndarray) and len(pt1.shape) != 1:
+            raise RuntimeError("passed kernel a %s-D pt" % pt1.shape)
+
+        if isinstance(pt1, np.ndarray) and pt2_list.shape[1] != len(pt1):
+            raise RuntimeError("passed kernel a %s-D point " % pt1.shape
+                               + " and a %s-D list of points" % pt2_list.shape)
+
+        return self._eval(pt1, pt2_list)
+
+
+class ExpSquaredKernel(KernelBase):
+
+    def __init__(self, dim=1):
+        """
+        dim=1 means there is one length scale hyper parameter
+        dim=N means that each dimension has its own length scale
+        """
+        self._n_hyper_params = dim
+        self._hyper_params = np.ones(dim, dtype=np.float)
+
+    def _eval(self, pt1, pt2_list):
+        """
+        pt1 is a single point
+
+        pt2 is a list of points
+        """
+        if isinstance(pt1, float):
+            ddsq = np.power((pt1-pt2_list)/self._hyper_params,2)
+        else:
+            ddsq = np.power((pt1-pt2_list)/self._hyper_params,2).sum(axis=1)
+
+        return np.exp(-0.5*ddsq)
+
+
+class CovariogramBase(object):
+
+    @property
+    def kriging_param(self):
+        return self._kriging_param
+
+    @property
+    def kernel(self):
+        return self._kernel
+
+    @property
+    def covar(self):
+        return self._covar
+
+    @property
+    def covar_inv(self):
+        return self._covar_inv
+
+    @property
+    def nugget(self):
+        return self._nugget
+
+    @nugget.setter
+    def nugget(self, val):
+        if not isinstance(val, float):
+            raise RuntimeError("Nugget should be a float; "
+                               "passing %s" % type(val))
+        self._nugget = val
+
+    @property
+    def hyper_params(self):
+        return np.append(kernel.hyper_params, self._kriging_param)
+
+    @hyper_params.setter
+    def hyper_params(self, val):
+        if len(val) != self._n_hyper_params:
+            raise RuntimeError("Trying to give "
+                               + "covariogram %d hyper params; " % len(val)
+                               + "it expects %d" % self._n_hyper_params)
+
+        self._kernel.hyper_params = val[:-1]
+        self._kriging_params = val[-1]
+
+    @property
+    def n_hyper_params(self):
+        return self._n_hyper_params
+
+
+class Covariogram(CovariogramBase):
+
+    def __init__(self, kernel):
+        self._n_hyper_params = kernel.n_hyper_params + 1
+        self._kriging_param = 1.0
+        self._kernel = kernel
+        self._nugget = 1.0e-5
+        self._covar = None
+        self._covar_inv = None
+
+    def __call__(self, pts_in):
+        """
+        pts_in should be a 2-D numpy array.  Each row is one of the points
+        in parameter space on which we are building the Gaussian Process.
+
+        If we are just constructing a 1-D Gaussian Process, this can be
+        a 1-D numpy array in which each point is the abscissa of our
+        data points.
+        """
+        if not isinstance(pts_in, np.ndarray):
+            raise RuntimeError("Need to pass a numpy array to Covariogram()")
+
+        self._covar = []
+        for ix, pt in enumerate(pts_in):
+            kernel_vals = self.kernel(pt, pts_in)
+            kernel_vals[ix] += self.nugget
+            self._covar.append(kernel_vals)
+
+        self._covar = np.array(self._covar)/self.kriging_param
+        self._covar_inv = np.linalg.inv(self._covar)
---
+++ lsst_sims/test_gp.py
@@ -0,0 +1,89 @@
+import unittest
+import numpy as np
+
+from gaussian_process import ExpSquaredKernel
+
+class ExpSquaredKernelTest(unittest.TestCase):
+    """
+    Test that the class implementing the squared exponential
+    kernel works properly
+    """
+
+    def test_on_scalars(self):
+        """
+        Test on data that is just scalars
+        """
+        kernel = ExpSquaredKernel()
+        self.assertTrue(kernel.n_hyper_params, 1)
+        self.assertEqual(kernel.hyper_params[0], 1.0)
+        ans = kernel(8.2, np.array([9.0]))
+        control = np.exp(-0.5*np.array([0.8*0.8]))
+        np.testing.assert_array_almost_equal(ans/control, np.ones(1), decimal=10)
+
+        pt2_list = np.array([1.3, 1.2, 1.4])
+        ans = kernel(1.5, pt2_list)
+        control = np.exp(-0.5*np.array([0.04, 0.09, 0.01]))
+        np.testing.assert_array_almost_equal(ans/control, np.ones(3), decimal=10)
+
+        kernel.hyper_params = 0.53
+        ans = kernel(8.3, np.array([5.4]))
+        control = np.exp(-0.5*np.array([np.power(2.9/0.53,2)]))
+        np.testing.assert_array_almost_equal(ans/control, np.ones(1), decimal=10)
+
+        ans = kernel(2.3, np.array([1.1, 2.5, 3.7]))
+        control = np.exp(-0.5*np.power(np.array([1.2, 0.2, 1.4])/0.53,2))
+        np.testing.assert_array_almost_equal(ans/control, np.ones(3), decimal=10)
+
+
+    def test_on_vectors(self):
+        """
+        Test on mutli-dimensional points (but only one length scale)
+        """
+        kernel = ExpSquaredKernel()
+        self.assertTrue(kernel.n_hyper_params, 1)
+        self.assertEqual(kernel.hyper_params[0], 1.0)
+
+        pt1 = np.array([1.1, 2.4, 5.6])
+
+        pt2_list = np.array([[2.3, 3.1, 5.5]])
+        ans = kernel(pt1, pt2_list)
+        control = np.array([np.exp(-0.5*(1.2*1.2+0.7*0.7+0.01))])
+        np.testing.assert_array_almost_equal(ans/control, np.ones(1), decimal=10)
+
+        pt2_list = np.array([[1.1, 1.1, 1.1], [2.4, 2.4, 2.4]])
+        ans = kernel(pt1, pt2_list)
+        control = np.exp(-0.5*np.array([1.3*1.3+4.5*4.5, 1.3*1.3+3.2*3.2]))
+        np.testing.assert_array_almost_equal(ans/control, np.ones(2), decimal=10)
+
+        kernel.hyper_params = 2.0
+        self.assertEqual(kernel.hyper_params, 2.0)
+
+        pt2_list = np.array([[2.3, 3.1, 5.5]])
+        ans = kernel(pt1, pt2_list)
+        control = np.array([np.exp(-0.5*(1.2*1.2+0.7*0.7+0.01)*0.25)])
+        np.testing.assert_array_almost_equal(ans/control, np.ones(1), decimal=10)
+
+        pt2_list = np.array([[1.1, 1.1, 1.1], [2.4, 2.4, 2.4]])
+        ans = kernel(pt1, pt2_list)
+        control = np.exp(-0.5*np.array([1.3*1.3+4.5*4.5, 1.3*1.3+3.2*3.2])*0.25)
+        np.testing.assert_array_almost_equal(ans/control, np.ones(2), decimal=10)
+
+    def test_multi_length_scales(self):
+        kernel = ExpSquaredKernel(dim=3)
+        kernel.hyper_params = np.array([1.0, 2.0, 3.0])
+        np.testing.assert_array_equal(kernel.hyper_params, np.array([1.0, 2.0, 3.0]))
+
+        pt1 = np.array([2.3, 5.6, 8.1])
+        pt2_list = np.array([[1.1, 3.4, 7.2]])
+        ans = kernel(pt1, pt2_list)
+        control = np.exp(-0.5*(1.2*1.2+0.25*2.2*2.2+0.9*0.9/9.0))
+        np.testing.assert_array_almost_equal(ans/control, np.ones(1), decimal=10)
+
+        pt2_list = np.array([[1.1, 1.1, 1.1], [2.2, 2.2, 2.2]])
+        ans = kernel(pt1, pt2_list)
+        control = np.exp(-0.5*np.array([1.2*1.2+0.25*4.5*4.5+7.0*7.0/9.0,
+                                        0.1*0.1+3.4*3.4*0.25+5.9*5.9/9.0]))
+        np.testing.assert_array_almost_equal(ans/control, np.ones(2), decimal=10)
+
+if __name__ == "__main__":
+    unittest.main()
