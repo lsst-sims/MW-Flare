98fdb01d06572bff5d9fd51b264ff4a6df0f7df8
danielsf <scott.f.daniel@gmail.com>
2017-02-06 16:36:24 -0800

    do not anchor in fitting script

--- lsst_sims/fit_all_bins.sh
+++ lsst_sims/fit_all_bins.sh
@@ -1,26 +1,26 @@
 python fit_zbin.py --infile z_bins/bin_0_25.txt \
---outfile z_bin_fits/gp_0_25.txt --anchor 0.0 0.0 10.0 0.95 0.95
+--outfile z_bin_fits/gp_0_25.txt
 
 python fit_zbin.py --infile z_bins/bin_25_50.txt \
---outfile z_bin_fits/gp_25_50.txt --anchor 0.0 0.0 11.0 0.95 0.95
+--outfile z_bin_fits/gp_25_50.txt
 
 python fit_zbin.py --infile z_bins/bin_50_75.txt \
---outfile z_bin_fits/gp_50_75.txt --anchor 0.0 0.0 10.0 0.95 0.95
+--outfile z_bin_fits/gp_50_75.txt
 
 python fit_zbin.py --infile z_bins/bin_75_100.txt \
---outfile z_bin_fits/gp_75_100.txt --anchor 0.0 0.0 12.0 0.75 0.75
+--outfile z_bin_fits/gp_75_100.txt
 
 python fit_zbin.py --infile z_bins/bin_100_125.txt \
---outfile z_bin_fits/gp_100_125.txt --anchor 0.0 0.0 9.0 0.0 0.0
+--outfile z_bin_fits/gp_100_125.txt
 
 python fit_zbin.py --infile z_bins/bin_125_150.txt \
---outfile z_bin_fits/gp_125_150.txt --anchor 0.0 0.0 10.0 0.75 0.75
+--outfile z_bin_fits/gp_125_150.txt
 
 python fit_zbin.py --infile z_bins/bin_150_175.txt \
---outfile z_bin_fits/gp_150_175.txt --anchor 0.0 0.0 10.0 0.0 0.0
+--outfile z_bin_fits/gp_150_175.txt
 
 python fit_zbin.py --infile z_bins/bin_175_200.txt \
---outfile z_bin_fits/gp_175_200.txt --anchor 0.0 0.0 10.0 0.0 0.0
+--outfile z_bin_fits/gp_175_200.txt
 
 python fit_zbin.py --infile z_bins/bin_200_225.txt \
---outfile z_bin_fits/gp_200_225.txt --anchor 0.0 0.0 10.0 0.0 0.0
+--outfile z_bin_fits/gp_200_225.txt
