eadb92dad1044755e15520ec13b5f42f30f46119
danielsf <scott.f.daniel@gmail.com>
2017-03-22 11:35:25 -0700

    vaildate_amplitude.py round-trips real data

--- lsst_sims/validate_amplitude.py
+++ lsst_sims/validate_amplitude.py
@@ -9,43 +9,75 @@ import matplotlib.pyplot as plt
 
 from mdwarf_utils import duration_from_energy, amplitude_from_duration_energy
 
+from plot_utils import make_distribution_plot, make_density_plot
+
 import numpy as np
 
-rng = np.random.RandomState(77)
+dtype = np.dtype([('start_dex', float), ('stop_dex', float),
+                  ('start', float), ('stop', float), ('peak', float),
+                  ('rise', float), ('decay', float), ('amp', float),
+                  ('e_dur', float), ('e_dur_rise', float),
+                  ('e_dur_decay', float), ('flag', float),
+                  ('ppl_flare', float), ('ppl_month', float),
+                  ('components', float)])
 
-n_flares = 10000
-log_e = rng.random_sample(n_flares)*4.0 + 29.0
-ee = np.power(10.0, log_e)
+control_data = np.genfromtxt('data/gj1243_master_flares.tbl', dtype=dtype)
+valid = np.where(np.logical_not(np.isnan(control_data['amp'])))
+control_data = control_data[valid]
+valid = np.where(np.logical_and(control_data['amp']>0.0, control_data['e_dur']>0.0))
+control_data = control_data[valid]
 
-duration = duration_from_energy(ee, rng)
-amplitude_u = amplitude_from_duration_energy(duration, ee)
+log_ekp_quiescent = 30.67
 
-log_ekp_quiescent = 30.67 # GJ1243 in Table 2 of Hawley et al 2014
-ekp_quiescent = np.power(10.0, log_ekp_quiescent)
+control_log_ekp = log_ekp_quiescent + np.log10(control_data['e_dur'])
+
+log_eu = control_log_ekp + np.log10(0.65)
+
+eu = np.power(10.0, log_eu)
+rng = np.random.RandomState(88)
+
+duration = duration_from_energy(eu, rng)
+amplitude_u = amplitude_from_duration_energy(duration, eu)
 
 # Convert to Kepler amplitude.
 # See paragraph before section 3 of Hawley et al 2014
 amplitude_kp = amplitude_u/0.65 
 
+amp_rel = amplitude_kp/np.power(10.0, log_ekp_quiescent)
+
+dx=0.05
 plt.figsize = (30, 30)
-plt.subplot(1,2,1)
-plt.scatter(np.log10(ee/0.65), amplitude_kp/ekp_quiescent)
-plt.xlabel('Log(E_kp) in ergs')
-plt.ylabel('relative amplitude')
-plt.yscale('log')
-plt.xlim(29, 33)
-plt.xticks(range(29,34))
-plt.ylim(0.0005, 0.5)
+plt.subplot(2,2,1)
+make_density_plot(control_log_ekp, np.log10(amp_rel), dx, cmax=40, dc=10)
+plt.xlabel('Log(E_kp)')
+plt.ylabel('Log(simulated amplitude)')
+plt.xlim(27, 34)
+plt.ylim(-5, 1)
+
+plt.subplot(2,2,2)
+make_density_plot(control_log_ekp, np.log10(control_data['amp']), dx,
+                  cmax=40, dc=10)
+plt.xlabel('Log(E_kp)')
+plt.ylabel('Log(amplitude')
+plt.xlim(27, 34)
+plt.ylim(-5, 1)
+
+plt.subplot(2,2,3)
+hh, = make_distribution_plot(np.log10(amp_rel), dx, color='k')
+header_list = [hh]
+label_list = ['simulation']
+hh, = make_distribution_plot(np.log10(control_data['amp']), dx, color='r')
+header_list.append(hh)
+label_list.append('data')
+plt.xlabel('Log(amplitude)')
+plt.ylabel('N')
+plt.legend(header_list, label_list, loc=0, fontsize=10)
 
 
-plt.subplot(1,2,2)
-plt.scatter(duration, amplitude_kp/ekp_quiescent)
-plt.xlabel('duration (minutes)')
-plt.ylabel('relative amplitude')
-plt.xscale('log')
-plt.yscale('log')
-plt.xlim(1,200)
-plt.ylim(0.0005, 0.5)
-
+plt.subplot(2,2,4)
+make_distribution_plot((control_data['amp']-amp_rel)/control_data['amp'],
+                       dx)
+plt.xlabel('(amp - simulated amp)/amp')
+plt.ylabel('N')
 plt.tight_layout()
 plt.savefig('plots/amplitude_plot.png')
