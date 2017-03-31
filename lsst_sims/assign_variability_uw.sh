4f650da547c75de85a500d0c32a72c8baf232174
danielsf <scott.f.daniel@gmail.com>
2017-03-31 15:10:52 -0700

    nice all of the assignment runs in bash script

--- lsst_sims/assign_variability_uw.sh
+++ lsst_sims/assign_variability_uw.sh
@@ -2,36 +2,36 @@
 
 output_dir=/astro/store/pogo4/danielsf/mlt_flares/
 
-python assign_varParamStr.py --table stars_mlt_part_0870 \
+nohup nice python assign_varParamStr.py --table stars_mlt_part_0870 \
 --out_dir ${output_dir} --chunk_size 100000 --seed 88 &
 
 sleep 5
 
-python assign_varParamStr.py --table stars_mlt_part_1100 \
+nohup nice python assign_varParamStr.py --table stars_mlt_part_1100 \
 --out_dir ${output_dir} --chunk_size 100000 --seed 112 &
 
 sleep 5
 
-python assign_varParamStr.py --table stars_mlt_part_1160 \
+nohup nice python assign_varParamStr.py --table stars_mlt_part_1160 \
 --out_dir ${output_dir} --chunk_size 100000 --seed 321 &
 
 sleep 5
 
-python assign_varParamStr.py --table stars_mlt_part_1180 \
+nohup nice python assign_varParamStr.py --table stars_mlt_part_1180 \
 --out_dir ${output_dir} --chunk_size 100000 --seed 425 &
 
 sleep 5
 
-python assign_varParamStr.py --table stars_mlt_part_1220 \
+nohup nice python assign_varParamStr.py --table stars_mlt_part_1220 \
 --out_dir ${output_dir} --chunk_size 100000 --seed 6782 &
 
 sleep 5
 
-python assign_varParamStr.py --table stars_mlt_part_1250 \
+nohup nice python assign_varParamStr.py --table stars_mlt_part_1250 \
 --out_dir ${output_dir} --chunk_size 100000 --seed 371 &
 
 sleep 5
 
-python assign_varParamStr.py --table stars_mlt_part_1400 \
---out_dir ${output_dir} --chunk_size 100000 --seed 775 &
+nohup nice python assign_varParamStr.py --table stars_mlt_part_1400 \
+--out_dir ${output_dir} --chunk_size 100000 --seed 775
 
