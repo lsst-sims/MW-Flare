b4ffe3178739370d8453906d73ea094be76cea7d
danielsf <scott.f.daniel@gmail.com>
2017-03-31 23:10:47 -0700

    change flare light curve file name

--- lsst_sims/generate_many_light_curves.py
+++ lsst_sims/generate_many_light_curves.py
@@ -29,5 +29,5 @@ for star_class in ('early_active', 'early_inactive',
         print(star_class,ix,(time.time()-t_start)/60.0)
 
 
-with open('output/npz_light_curves.npz', 'wb') as file_handle:
+with open('output/mdwarf_flare_light_curves.npz', 'wb') as file_handle:
     np.savez(file_handle, **cache)
