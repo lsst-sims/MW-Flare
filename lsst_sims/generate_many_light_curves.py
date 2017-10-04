aea601f49e635acbae99a61bb0e4badeb8ed02cc
danielsf <scott.f.daniel@gmail.com>
2017-10-04 13:37:02 -0700

    don't use multiprocessing if n_proc is 1

--- lsst_sims/generate_many_light_curves.py
+++ lsst_sims/generate_many_light_curves.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 
 from mdwarf_utils import light_curve_from_params, generate_light_curve_params
+from mdwarf_utils import light_curve_from_class
 import time
 
 import multiprocessing as mproc
@@ -64,28 +65,37 @@ process_list = []
 for star_class in ('early_active', 'early_inactive', 'mid_active',
                    'mid_inactive', 'late_active'):
 
-
     for ix in range(4):
         tag = '%s_%d' % (star_class, ix)
 
-        (t_peak_arr,
-         fwhm_arr,
-         amplitude_arr) = generate_light_curve_params(star_class, args.duration, rng)
+        if args.n_proc == 1:
+            (cache['%s_time' % tag],
+             cache['%s_u' % tag],
+             cache['%s_g' % tag],
+             cache['%s_r' % tag],
+             cache['%s_i' % tag],
+             cache['%s_z' % tag],
+             cache['%s_y' % tag]) = light_curve_from_class(star_class, args.duration, rng)
+        else:
+            (t_peak_arr,
+             fwhm_arr,
+             amplitude_arr) = generate_light_curve_params(star_class, args.duration, rng)
 
-        p = mproc.Process(target=cache_lc,
-                          kwargs={'t_peak_arr': t_peak_arr,
-                                  'fwhm_arr': fwhm_arr,
-                                  'amp_arr': amplitude_arr,
-                                  'lock': lock,
-                                  'out_dict': cache,
-                                  'tag': tag})
-        p.start()
-        process_list.append(p)
-        if len(process_list) >= args.n_proc:
-            for p in process_list:
-                p.join()
-            print('did batch in %e' % ((time.time()-t_start)/60.0))
-            process_list = []
+
+            p = mproc.Process(target=cache_lc,
+                              kwargs={'t_peak_arr': t_peak_arr,
+                                      'fwhm_arr': fwhm_arr,
+                                      'amp_arr': amplitude_arr,
+                                      'lock': lock,
+                                      'out_dict': cache,
+                                      'tag': tag})
+            p.start()
+            process_list.append(p)
+            if len(process_list) >= args.n_proc:
+                for p in process_list:
+                    p.join()
+                print('did batch in %e' % ((time.time()-t_start)/60.0))
+                process_list = []
 
 for p in process_list:
     p.join()
